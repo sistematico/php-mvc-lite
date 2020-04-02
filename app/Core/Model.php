e87216274731c66880735d83a0837a316313f63e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-04-02 11:15:47 -0400

    Changed Model.php to use SQLite instead MySQL

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -1,48 +1,26 @@
 <?php
 
 namespace App\Core;
-
 use PDO;
 
 class Model
 {
-    /**
-     * @var null Database Connection
-     */
     public $db = null;
 
-    /**
-     * Whenever model is created, open a database connection.
-     */
-    function __construct()
-    {
+    function __construct() {
         try {
-            self::openDatabaseConnection();
+            self::open();
         } catch (\PDOException $e) {
-            exit('Database connection could not be established.');
+            exit('A conexão com o banco de dados não pode ser feita.');
         }
     }
 
-    /**
-     * Open the database connection with the credentials from app/config/config.php
-     */
-    private function openDatabaseConnection()
-    {
-        // set the (optional) options of the PDO connection. in this case, we set the fetch mode to
-        // "objects", which means all results will be objects, like this: $result->user_name !
-        // For example, fetch mode FETCH_ASSOC would return results like this: $result["user_name] !
-        // @see http://www.php.net/manual/en/pdostatement.fetch.php
-        $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
-        
-        // setting the encoding is different when using PostgreSQL
-        if (DB_TYPE == "pgsql") {
-            $databaseEncodingenc = " options='--client_encoding=" . DB_CHARSET . "'";
-        } else {
-            $databaseEncodingenc = "; charset=" . DB_CHARSET;
-        }
+    private function open() {
+        if (!is_dir(dirname(DB_FILE)))
+            mkdir(dirname(DB_FILE), 0755);
 
-        // generate a database connection, using the PDO connector
-        // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
-        $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . $databaseEncodingenc, DB_USER, DB_PASS, $options);
+        $this->db = new PDO('sqlite:' . DB_FILE);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
     }
 }
