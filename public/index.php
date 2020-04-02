a3acfb116e061d0da61c3823e31045c7e6fecaca
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-04-02 11:09:26 -0400

    Changed index.php title

--- public/index.php
+++ public/index.php
@@ -1,28 +1,7 @@
 <?php
 
-/**
- * MINI - an extremely simple naked PHP application
- *
- * @package mini
- * @author Panique
- * @link http://www.php-mini.com
- * @link https://github.com/panique/mini/
- * @license http://opensource.org/licenses/MIT MIT License
- */
-
-/**
- * Now MINI work with namespaces + composer's autoloader (PSR-4)
- *
- * @author Joao Vitor Dias <joaodias@noctus.org>
- *
- * For more info about namespaces plase @see http://php.net/manual/en/language.namespaces.importing.php
- */
-
-// set a constant that holds the project's folder path, like "/var/www/".
-// DIRECTORY_SEPARATOR adds a slash to the end of the path
 define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
-// set a constant that holds the project's "application" folder, like "/var/www/application".
-define('APP', ROOT . 'application' . DIRECTORY_SEPARATOR);
+define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
 
 // This is the auto-loader for Composer-dependencies (to load tools into your project).
 require ROOT . 'vendor/autoload.php';
@@ -31,7 +10,7 @@ require ROOT . 'vendor/autoload.php';
 require APP . 'config/config.php';
 
 // load application class
-use Mini\Core\Application;
+use App\Core\Application;
 
 // start the application
 $app = new Application();
