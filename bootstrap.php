cfbb130676caaf04545bb9fc7cc7d1a24ba56ae2
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-04-08 20:18:17 -0400

    Updated bootstrap.php to check if file exists before copy or move

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,31 @@
 <?php
 
-rename('vendor/components/font-awesome/webfonts/', 'public/webfonts/');
-copy('vendor/twbs/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
-copy('vendor/twbs/bootstrap/dist/css/bootstrap.min.css.map', 'public/css/bootstrap.min.css.map');
-copy('vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js', 'public/js/bootstrap.bundle.min.js');
-copy('vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js.map', 'public/js/bootstrap.bundle.min.js.map');
-copy('vendor/components/jquery/jquery.min.js', 'public/js/jquery.min.js');
-copy('vendor/components/font-awesome/css/all.min.css', 'public/css/fontawesome.min.css');
+if (file_exists('vendor/components/font-awesome/webfonts/')) {
+    rename('vendor/components/font-awesome/webfonts/', 'public/webfonts/');
+}
+
+if (file_exists('vendor/twbs/bootstrap/dist/css/bootstrap.min.css')) {
+    copy('vendor/twbs/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
+}
+
+if (file_exists('vendor/twbs/bootstrap/dist/css/bootstrap.min.css.map')) {
+    copy('vendor/twbs/bootstrap/dist/css/bootstrap.min.css.map', 'public/css/bootstrap.min.css.map');
+}
+
+if (file_exists('vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js')) {
+    copy('vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js', 'public/js/bootstrap.bundle.min.js');
+}
+
+if (file_exists('vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js.map')) {
+    copy('vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js.map', 'public/js/bootstrap.bundle.min.js.map');
+}
+
+if (file_exists('vendor/components/jquery/jquery.min.js')) {
+    copy('vendor/components/jquery/jquery.min.js', 'public/js/jquery.min.js');
+}
+
+if (file_exists('vendor/components/font-awesome/css/all.min.css')) {
+    copy('vendor/components/font-awesome/css/all.min.css', 'public/css/fontawesome.min.css');
+}
+
+?>
