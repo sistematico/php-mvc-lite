32015d55295f12e699bc77b09bf396d1a36488e5
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-04-13 16:21:36 -0400

    Updated error page

--- app/view/error/index.php
+++ app/view/error/index.php
@@ -1,3 +1,6 @@
-<div class="container">
-    <p>This is the Error-page. Will be shown when a page (= controller / method) does not exist.</p>
-</div>
+<main role="main" class="container">
+    <div class="starter-template">
+        <h1>Erro</h1>
+        <p class="lead">This is the Error-page. Will be shown when a page (= controller / method) does not exist.</p>
+    </div>
+</main><!-- /.container -->
