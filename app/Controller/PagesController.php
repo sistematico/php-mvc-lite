96180884821882e3dbdd97d12bcba7a41fcb086b
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-04-14 11:08:10 -0400

    Changed PagesController.php

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -8,21 +8,21 @@ class PagesController
     public function index()
     {
         require APP . 'view/_templates/header.php';
-        require APP . 'views/pages/index.php';
+        require APP . 'view/pages/index.php';
         require APP . 'view/_templates/footer.php';
     }
 
     public function install()
     {
         require APP . 'view/_templates/header.php';
-        require APP . 'views/pages/example_one.php';
+        require APP . 'view/pages/example_one.php';
         require APP . 'view/_templates/footer.php';
     }
 
     public function exampleTwo()
     {
         require APP . 'view/_templates/header.php';
-        require APP . 'views/pages/example_two.php';
+        require APP . 'view/pages/example_two.php';
         require APP . 'view/_templates/footer.php';
     }
 
