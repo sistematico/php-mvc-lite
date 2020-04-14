db2438d623cd71fe5662e64d7e0f79f0b8e151ce
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-04-14 13:31:21 -0400

    Changed folder of error.php

--- app/view/pages/error.php
+++ app/view/pages/error.php
@@ -1,6 +1,6 @@
-<main role="main" class="container">
-    <div class="starter-template">
-        <h1>Erro</h1>
+<main role="main" class="flex-shrink-0">
+    <div class="container">
+        <h1 class="mt-5">Erro</h1>
         <p class="lead">This is the Error-page. Will be shown when a page (= controller / method) does not exist.</p>
     </div>
 </main><!-- /.container -->
