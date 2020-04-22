a296d89ac8a85b93e4b8629082213cd7c65d8aea
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-04-22 19:45:33 -0400

    Master update

--- songs.sql
+++ songs.sql
@@ -3,3 +3,5 @@ INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (2, 'Milionário & J
 INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (3, 'Chrystian & Ralf', 'Sozinho em Nova York', 'http://www.chrystianeralf.com.br');
 INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (4, 'Henrique e Juliano', 'Garrafas Vazias', '');
 INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (5, 'Gusttavo Lima', 'Agente Fez Amor', 'https://www.youtube.com/watch?v=S2OI1CtnfUM');
+INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (6, 'Marília Mendonça', 'Tentativas', '');
+INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (7, 'Di Paullo & Paulino', 'Nada Mudou', '');
