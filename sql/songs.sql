120f76033f8d101c6c4ee3d2f1b5f1eecdd0f028
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-06-20 14:11:18 -0400

    Added sql folder

---
+++ sql/songs.sql
@@ -0,0 +1,7 @@
+INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (1, 'Zezé Di Camargo & Luciano', 'Boa Sorte', 'http://zezedicamargoeluciano.com.br');
+INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (2, 'Milionário & José Rico', 'Decida', '');
+INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (3, 'Chrystian & Ralf', 'Sozinho em Nova York', 'http://www.chrystianeralf.com.br');
+INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (4, 'Henrique e Juliano', 'Garrafas Vazias', '');
+INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (5, 'Gusttavo Lima', 'Agente Fez Amor', 'https://www.youtube.com/watch?v=S2OI1CtnfUM');
+INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (6, 'Marília Mendonça', 'Tentativas', '');
+INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (7, 'Di Paullo & Paulino', 'Nada Mudou', '');
