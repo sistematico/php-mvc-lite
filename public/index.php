197fa0ef039bd759f504c2a17fc3f0a31d4b7b93
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-17 08:21:01 -0400

    Autocommit por GitHub.sh: alterações: add 'public/index.php'

--- public/index.php
+++ public/index.php
@@ -9,17 +9,13 @@ error_reporting(E_ALL);
 define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
 define('DB_FILE', ROOT . 'db' . DIRECTORY_SEPARATOR . 'banco.sqlite');
+define('SQL_FILE', ROOT . 'sql' . DIRECTORY_SEPARATOR . 'database.sql');
 
-// This is the auto-loader for Composer-dependencies (to load tools into your project).
 require ROOT . 'vendor/autoload.php';
 
-// load application config (error reporting etc.)
 if (file_exists(APP . 'config/config.php')) {
     require APP . 'config/config.php';
 }
 
-// load application class
 use App\Core\Application;
-
-// start the application
 $app = new Application();
