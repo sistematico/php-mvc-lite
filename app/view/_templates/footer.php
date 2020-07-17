78891605bc4ccca33bd5f5c4710441c26e532add
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-17 10:15:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/_templates/footer.php'

--- app/view/_templates/footer.php
+++ app/view/_templates/footer.php
@@ -1,10 +1,11 @@
 <footer class="footer mt-auto py-3 bg-light">
-  <div class="container">
-  <span class="text-muted">
-            Made with <i class="fas fa-heart"></i> by <a href="https://lucasbrum.net" target="_blank">Lucas</a> in Terenos. <div class="d-none d-sm-inline">Sources on <a href="https://github.com/sistematico/php-mvc-lite" target="_blank">Github</a>.
+    <div class="container">
+        <span class="text-muted">
+            Made with <i class="fas fa-heart"></i> by <a href="https://lucasbrum.net" target="_blank">Lucas</a> in
+            Terenos. <div class="d-none d-sm-inline">Sources on <a href="https://github.com/sistematico/php-mvc-lite" target="_blank">Github</a>.
             Proudly hosted by <a href="https://www.owned-networks.net/client_area/aff.php?aff=11" target="_blank">Owned-Networks</a>.</div>
         </span>
-  </div>
+    </div>
 </footer>
 <script src="<?php echo URL; ?>js/bootstrap.bundle.min.js"></script>
 <script>let url = "<?php echo URL; ?>";</script>
