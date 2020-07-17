807065ad809adcdad91ef1e1fe9385b0c5540aef
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-17 08:11:01 -0400

    Autocommit por GitHub.sh: alterações: remove 'public/whatsapp.html' add 'sql/database.sql'

--- sql/database.sql
+++ sql/database.sql
@@ -1,7 +1,14 @@
-INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (1, 'Zezé Di Camargo & Luciano', 'Boa Sorte', 'http://zezedicamargoeluciano.com.br');
-INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (2, 'Milionário & José Rico', 'Decida', '');
-INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (3, 'Chrystian & Ralf', 'Sozinho em Nova York', 'http://www.chrystianeralf.com.br');
-INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (4, 'Henrique e Juliano', 'Garrafas Vazias', '');
-INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (5, 'Gusttavo Lima', 'Agente Fez Amor', 'https://www.youtube.com/watch?v=S2OI1CtnfUM');
-INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (6, 'Marília Mendonça', 'Tentativas', '');
-INSERT OR IGNORE INTO song (id, artist, track, link) VALUES (7, 'Di Paullo & Paulino', 'Nada Mudou', '');
+CREATE TABLE IF NOT EXISTS "songs" (
+	"id"		INTEGER PRIMARY KEY AUTOINCREMENT,
+	"artist"	TEXT,
+	"track"		TEXT,
+	"link"		TEXT UNIQUE
+);
+
+INSERT OR IGNORE INTO "songs" (id, artist, track, link) VALUES (1, 'Zezé Di Camargo & Luciano', 'Boa Sorte', 'http://zezedicamargoeluciano.com.br');
+INSERT OR IGNORE INTO "songs" (id, artist, track, link) VALUES (2, 'Milionário & José Rico', 'Decida', '');
+INSERT OR IGNORE INTO "songs" (id, artist, track, link) VALUES (3, 'Chrystian & Ralf', 'Sozinho em Nova York', 'http://www.chrystianeralf.com.br');
+INSERT OR IGNORE INTO "songs" (id, artist, track, link) VALUES (4, 'Henrique e Juliano', 'Garrafas Vazias', '');
+INSERT OR IGNORE INTO "songs" (id, artist, track, link) VALUES (5, 'Gusttavo Lima', 'Agente Fez Amor', 'https://www.youtube.com/watch?v=S2OI1CtnfUM');
+INSERT OR IGNORE INTO "songs" (id, artist, track, link) VALUES (6, 'Marília Mendonça', 'Tentativas', '');
+INSERT OR IGNORE INTO "songs" (id, artist, track, link) VALUES (7, 'Di Paullo & Paulino', 'Nada Mudou', '');
