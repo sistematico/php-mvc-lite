f8badad3e29c2dad0ad522524485b519585647dd
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-17 11:34:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/billionaires/edit.php'

--- app/view/billionaires/edit.php
+++ app/view/billionaires/edit.php
@@ -20,7 +20,7 @@
         </div>
         <div class="col">
             <label for="money" class="sr-only">Money</label>
-            <input name="money" type="text" class="form-control" id="money" placeholder="Money" value="<?php echo htmlspecialchars($billionaire->money, ENT_QUOTES, 'UTF-8'); ?>" required />
+            <input name="money" type="number" class="form-control" id="money" placeholder="Money" min="1" max="5" value="<?php echo htmlspecialchars($billionaire->money, ENT_QUOTES, 'UTF-8'); ?>" required>
         </div>
         <div class="col">
             <label for="link" class="sr-only">Link</label>
