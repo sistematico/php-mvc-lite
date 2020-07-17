ca8098b95d3fac6be280c762a39d24e277e4f06f
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-17 10:32:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/songs/edit.php'

--- app/view/songs/edit.php
+++ app/view/songs/edit.php
@@ -12,21 +12,23 @@
     <h1>Songs</h1>
     <h3>Edit a song</h3>
 
-    <form class="form-inline" action="<?php echo URL; ?>songs/updatesong" method="post">
-        <div class="form-group mb-2 mr-2">
+    <form class="row g-3" action="<?php echo URL; ?>songs/updatesong" method="post">
+        <input type="hidden" name="song_id" value="<?php echo htmlspecialchars($song->id, ENT_QUOTES, 'UTF-8'); ?>" />
+        <div class="col">
             <label for="artist" class="sr-only">Artist</label>
             <input name="artist" type="text" class="form-control" id="artist" placeholder="Artist" value="<?php echo htmlspecialchars($song->artist, ENT_QUOTES, 'UTF-8'); ?>" required />
         </div>
-        <div class="form-group mb-2 mr-2">
+        <div class="col">
             <label for="track" class="sr-only">Track</label>
             <input name="track" type="text" class="form-control" id="track" placeholder="Track" value="<?php echo htmlspecialchars($song->track, ENT_QUOTES, 'UTF-8'); ?>" required />
         </div>
-        <div class="form-group mb-2 mr-2">
+        <div class="col">
             <label for="link" class="sr-only">Link</label>
             <input name="link" type="text" class="form-control" id="link" placeholder="Link" value="<?php echo htmlspecialchars($song->link, ENT_QUOTES, 'UTF-8'); ?>" />
         </div>
-        <input type="hidden" name="song_id" value="<?php echo htmlspecialchars($song->id, ENT_QUOTES, 'UTF-8'); ?>" />
-        <button name="updatesong" type="submit" class="btn btn-primary mb-2">Update</button>
+        <div class="col-1">
+            <button name="updatesong" type="submit" class="btn btn-primary mb-2">Update</button>
+        </div>
     </form>
     
 </div>
