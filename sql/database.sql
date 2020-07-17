de72ddba830e085914594bdd4f9d752193c4274c
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-17 11:05:01 -0400

    Autocommit por GitHub.sh: alterações: add 'sql/database.sql'

--- sql/database.sql
+++ sql/database.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS "billionaires" (
 );
 
 INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (1, 'Jeff Bezos', 113, 'http://zezedicamargoeluciano.com.br');
-INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (2, 'Milionário & José Rico', 'Decida', '');
+INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (2, 'Bill Gates', 54, '');
 INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (3, 'Chrystian & Ralf', 'Sozinho em Nova York', 'http://www.chrystianeralf.com.br');
 INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (4, 'Henrique e Juliano', 'Garrafas Vazias', '');
 INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (5, 'Gusttavo Lima', 'Agente Fez Amor', 'https://www.youtube.com/watch?v=S2OI1CtnfUM');
