276aeb77c019347013b16b9d23d42d711c3a451d
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-17 11:16:01 -0400

    Autocommit por GitHub.sh: alterações: remove 'db/chat.sqlite' add 'sql/database.sql'

--- sql/database.sql
+++ sql/database.sql
@@ -2,13 +2,14 @@ CREATE TABLE IF NOT EXISTS "billionaires" (
 	"id"		INTEGER PRIMARY KEY AUTOINCREMENT,
 	"name"		TEXT,
 	"money"		INTEGER,
-	"link"		TEXT UNIQUE
+	"link"		TEXT
 );
 
-INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (1, 'Jeff Bezos', 113, 'http://zezedicamargoeluciano.com.br');
-INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (2, 'Bill Gates', 98, '');
-INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (3, 'Bernard Arnault & family', 76, 'http://www.chrystianeralf.com.br');
-INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (4, 'Warren Buffett', 67, '');
-INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (5, 'Larry Ellison', 59, 'https://www.youtube.com/watch?v=S2OI1CtnfUM');
-INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (6, 'Amancio Ortega', 55, '');
-INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (7, 'Mark Zuckerberg', 54, '');
+INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (1, 'Jeff Bezos', 1130, '');
+INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (2, 'Bill Gates', 980, '');
+INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (3, 'Bernard Arnault & family', 760, '');
+INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (4, 'Warren Buffett', 675, '');
+INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (5, 'Larry Ellison', 590, '');
+INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (6, 'Amancio Ortega', 551, '');
+INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (7, 'Mark Zuckerberg', 547, '');
+INSERT OR IGNORE INTO "billionaires" (id, name,	money, link) VALUES (8, 'Jim Walton', 546, '');
