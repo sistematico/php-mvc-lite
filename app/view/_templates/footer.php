6e8a4bd7c55c75f4f9e12f0d5845f5e09243dc8d
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-17 10:09:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/_templates/footer.php' add 'public/css/bootstrap.min.css' add 'public/css/bootstrap.min.css.map'

--- app/view/_templates/footer.php
+++ app/view/_templates/footer.php
@@ -6,11 +6,7 @@
         </span>
     </div>
 </footer>
-<script src="<?php echo URL; ?>js/jquery.min.js"></script>
 <script src="<?php echo URL; ?>js/bootstrap.bundle.min.js"></script>
-<script>
-    var url = "<?php echo URL; ?>";
-</script>
-<script src="<?php echo URL; ?>js/application.js"></script>
+<script>let url = "<?php echo URL; ?>";</script>
 </body>
 </html>
