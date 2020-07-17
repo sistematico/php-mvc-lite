6be10dde0ef498d05e0a6d74eae5d2f21dde581f
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-17 08:38:01 -0400

    Autocommit por GitHub.sh: alterações: add 'app/view/songs/index.php'

--- app/view/songs/index.php
+++ app/view/songs/index.php
@@ -46,16 +46,16 @@
         <tbody>
             <?php foreach ($songs as $song) { ?>
             <tr>
-                <th scope="row"><?php if (isset($song->id)) echo htmlspecialchars($song->id, ENT_QUOTES, 'UTF-8'); ?></th>
-                <td><?php if (isset($song->artist)) echo htmlspecialchars($song->artist, ENT_QUOTES, 'UTF-8'); ?></td>
-                <td><?php if (isset($song->track)) echo htmlspecialchars($song->track, ENT_QUOTES, 'UTF-8'); ?></td>
+                <th scope="row"><?php if (isset($song['id'])) echo htmlspecialchars($song['id'], ENT_QUOTES, 'UTF-8'); ?></th>
+                <td><?php if (isset($song['artist'])) echo htmlspecialchars($song['artist'], ENT_QUOTES, 'UTF-8'); ?></td>
+                <td><?php if (isset($song['track'])) echo htmlspecialchars($song['track'], ENT_QUOTES, 'UTF-8'); ?></td>
                 <td>
-                    <?php if (isset($song->link)) { ?>
-                        <a href="<?php echo htmlspecialchars($song->link, ENT_QUOTES, 'UTF-8'); ?>" target="_blank"><?php echo htmlspecialchars($song->link, ENT_QUOTES, 'UTF-8'); ?></a>
+                    <?php if (isset($song['link'])) { ?>
+                        <a href="<?php echo htmlspecialchars($song['link'], ENT_QUOTES, 'UTF-8'); ?>" target="_blank"><?php echo htmlspecialchars($song['link'], ENT_QUOTES, 'UTF-8'); ?></a>
                     <?php } ?>
                 </td>
-                <td><a onClick="javascript: return confirm('Are you sure you want to delete?');" href="<?php echo URL . 'songs/deletesong/' . htmlspecialchars($song->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-trash"></i></a></td>
-                <td><a href="<?php echo URL . 'songs/editsong/' . htmlspecialchars($song->id, ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-edit"></i></a></td>
+                <td><a onClick="javascript: return confirm('Are you sure you want to delete?');" href="<?php echo URL . 'songs/deletesong/' . htmlspecialchars($song['id'], ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-trash"></i></a></td>
+                <td><a href="<?php echo URL . 'songs/editsong/' . htmlspecialchars($song['id'], ENT_QUOTES, 'UTF-8'); ?>"><i class="fas fa-edit"></i></a></td>
             </tr>
             <?php } ?>
         </tbody>
