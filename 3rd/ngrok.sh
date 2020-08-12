2c3fe80c35b1e1a7fb327965061432ff51bfb483
Lucas Salies Brum <lucas@archlinux.com.br>
2020-08-11 23:20:01 -0400

    Autocommit por GitHub: alterações: add '3rd/ngrok.sh'

--- 3rd/ngrok.sh
+++ 3rd/ngrok.sh
@@ -2,6 +2,11 @@
 #
 # Source: https://gist.github.com/ardyantohermawan/04f3f79b17399949e24e18c653595902
 
+#if command -v unzip; then
+#    echo "Needs unzip package!"
+#    exit 1
+#fi
+
 # create directory
 sudo mkdir -p /opt/ngrok
 
@@ -9,16 +14,21 @@ sudo mkdir -p /opt/ngrok
 export TOKEN=$(cat ngrok.token)
 
 # create config file
-echo "authtoken: $TOKEN" | sudo tee /opt/ngrok/ngrok.yml
+#echo "authtoken: $TOKEN" | sudo tee /opt/ngrok/ngrok.yml
+
+echo "authtoken: $TOKEN
+tunnels:
+  http:
+    proto: http
+    addr: 80
+log: /opt/ngrok/ngrok.log" | sudo tee /opt/ngrok/ngrok.yml
+
 
 # change to tmp folder
 cd /tmp
 
-# download
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-
-# unzip
-unzip ngrok-stable-linux-amd64.zip 
+# download & unzip
+curl -s -L -O https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip 
 
 # move to /usr/local/bin
 sudo mv ngrok /usr/local/bin
@@ -39,7 +49,7 @@ WantedBy=multi-user.target" | sudo tee /etc/systemd/system/ngrok.service
 sudo systemctl daemon-reload
 
 # enable service
-sudo systemctl enable ngrok.service
+sudo systemctl --now enable ngrok.service
 
 # start service
 sudo systemctl start ngrok.service
